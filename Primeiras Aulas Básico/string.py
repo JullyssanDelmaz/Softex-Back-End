66c98b6ef442ad45888fb826fa5d134140e0df74
Jullyssan Delmaz <jullydelmaz@yahoo.com.br>
2025-08-08 00:03:08 +0000

    Primeiro commit

---
+++ Primeiras Aulas Básico/string.py
@@ -0,0 +1,54 @@
+# Exercício 2
+print("Me chamo Jullyssan Delmaz")
+print("Tenho 29 anos")
+print("Sou aluna BFD na Softex Recife")
+
+print()
+print()
+print()
+
+# Exercício 3
+print("Nome: João \nIdade: 20 anos \nCurso: Python Básico")
+print()
+print()
+
+#Exercício 4
+nome = "Jullyssan"
+idade = 29
+curso = "Python Básico"
+print(f"Meu nome é {nome}, tenho {idade} anos e sou aluna do BFD em {curso}.")
+print()
+print()
+print()
+# Exercício 5
+item = "#"
+print(4*item)
+print(f"{item}  {item}\n{item}  {item}")
+print(4*item)
+
+#Exercício 6
+
+a = 10
+b = 2
+soma = a+b
+subtracao = a-b
+multiplicacao=a*b
+divisao=a/b
+divisao_inteira=a//b
+resto_divisao=a%b
+potencia=a**b
+print(f"A soma é: {soma} \nA subtração é:{subtracao}\nA multiplicação é:{multiplicacao}\nA divisão é:{divisao}\nA divisão inteira é:{divisao_inteira}\nO resto da divisão é:{resto_divisao}\nA potência é:{potencia}")
+print()
+print()
+print()
+print()
+
+#Exercício 7
+
+#a = float(input("Escreva o primeiro valor: "))
+#b = float(input("Escreva o segundo valor: "))
+print(f"A soma dos valores é {a+b}")
+print(f"A subtração dos valores é: {a-b}")
+print(f"A multiplicação dos valores é: {a*b}")
+print(f"A divisão dos valores é: {a/b}")
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Softex-Back-End
 Primeira aula no curso de Back-End da Softex (07/08/2025)
+Professor Frederico Favaro
