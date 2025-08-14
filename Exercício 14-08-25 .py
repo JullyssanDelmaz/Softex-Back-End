60b46954bbfba0ee4529c006d74b0ac2039a6e2f
Jullyssan Delmaz <jullydelmaz@yahoo.com.br>
2025-08-14 23:55:37 +0000

    Exercícios feitos

---
+++ Exercício 14-08-25 .py
@@ -0,0 +1,30 @@
+# Exercício 1
+alunos = ["Hadassa", 17, "Wheilon", 17, "Gabriela", 16, "William", 18]
+print(alunos)
+
+# Exercício 2
+disciplinas = ["Matemática", "Português", "Física", "Biologia"]
+disciplinas.append("Química")
+disciplinas.append("História")
+print(disciplinas)
+disciplinas.remove("Biologia")
+print(disciplinas)
+
+# Exercício 3
+idades = [14, 12, 17, 25, 29, 21]
+idades_atuais = idades[:]
+print(idades_atuais)
+
+# Exercício 4
+r = []
+numeros = [0, 1, 2, 3, 4, 5]
+for num in numeros:
+    r.append(5 * num)
+print(r)
+
+# Exercício 5
+a1 = [1,2,3,4,5,6]
+a2 = a1[3:5]
+print(a2)
+
+
