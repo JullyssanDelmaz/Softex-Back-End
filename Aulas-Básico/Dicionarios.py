41d70fb1c6bb24e6a8064a49faf6d324e76fc249
Jullyssan Delmaz <jullydelmaz@yahoo.com.br>
2025-08-27 23:42:15 +0000

    Exercício de Dicionários

---
+++ Aulas-Básico/Dicionarios.py
@@ -0,0 +1,40 @@
+#1
+# aluno = {"nome": "Jullyssan",
+#          "idade": 29,
+#          "nota": 7.5}
+
+#2
+# produto ={"nome":"Caneta",
+#           "preço": 2.5,
+#           "estoque": 100}
+# print(produto.get("nome"))
+# print(produto.get("estoque"))
+
+#3
+# pessoa = {"nome": "Carlos",
+#           "idade": 30}
+# pessoa["cidade"]="São Paulo"
+# print(pessoa)
+
+# #4
+# carro = {"marca": "Ford",
+#          "modelo": "Fiesta",
+#          "ano": 2010}
+# carro.pop("ano")
+# print(carro)
+
+
+# #5
+# contato = {"nome": "Ana",
+#            "email": "ana@email.com"}
+# if contato.get("telefone") == None:
+#     print("Não informado")
+
+
+#6
+palavras = ["maçã", "banana", "maçã", "laranja", "banana", "maçã"]
+
+
+#7
+d = {"a": 1, "b": 2, "c": 3}
+
