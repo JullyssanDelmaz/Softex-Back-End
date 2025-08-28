dfff429803961329b1ce89f8ddc85d58b62b7b68
Jullyssan Delmaz <jullydelmaz@yahoo.com.br>
2025-08-28 23:29:14 +0000

    Exercício

--- Aulas-Básico/Dicionarios.py
+++ Aulas-Básico/Dicionarios.py
@@ -32,10 +32,39 @@
 
 
 #6
-palavras = ["maçã", "banana", "maçã", "laranja", "banana", "maçã"]
+
+# palavras = ["maçã", "banana", "maçã", "laranja", "banana", "maçã"]
+# def palavras_repetidas(palavras):
+#     contagem = {}
+#     for elemento in palavras:
+#         contagem[elemento] = contagem.get(elemento, 0) + 1
+#     return contagem
+# dic_palavras = palavras_repetidas(palavras)
+# print(dic_palavras)
 
 
 
-#7
-d = {"a": 1, "b": 2, "c": 3}
 
+
+# #7
+# d = {"a": 1, "b": 2, "c": 3}
+# invertido_d = {valor: chave for chave, valor in d.items()}
+# print(invertido_d)
+
+
+# 8
+notas_alunos = {"Ana": [4,6,9],
+                "Bianca": [3,6,4],
+                "Camilly": [10,9,7]}
+def media_alunos(notas_alunos):
+   for valor in notas_alunos.items():
+      print(valor)
+      for nota in valor[1]:
+         soma = 0
+         soma+=nota
+      media = soma/3
+      print(f" {media :.2f}")
+
+    
+    
+    
