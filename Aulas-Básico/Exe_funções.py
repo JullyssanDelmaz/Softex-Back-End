40dc3b62cfcc1eaebaae927424f2ef0c0dc5b527
Jullyssan Delmaz <jullydelmaz@yahoo.com.br>
2025-09-09 21:49:20 +0000

    Exercício de Funções

---
+++ Aulas-Básico/Exe_funções.py
@@ -0,0 +1,12 @@
+# Questão 1
+
+def saudacao():
+    print("Olá, bem-vindo ao Python!")
+print(saudacao)
+
+# Questão 2
+
+# x = int(input("Escreva um valor: "))
+# def dobro(x):
+#     k = 2*x
+# return dobro
