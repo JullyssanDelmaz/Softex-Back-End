e4dae2e8937eb724a0548b6c37a8dcbc932fc9be
Jullyssan Delmaz <jullydelmaz@yahoo.com.br>
2025-10-02 23:01:29 +0000

    Exercício de Abstract

---
+++ Aulas-Básico/Aula 02-10-25.py
@@ -0,0 +1,103 @@
+from abc import ABC, abstractmethod
+
+# class Animal(ABC):
+#     @abstractmethod
+#     def movimentar(self,metros:float):
+#         print(f"O animal se deslocou {metros} metros")
+# animal1 = Animal()
+# animal1.movimentar(5)
+
+# class Pessoa(ABC):
+#     @abstractmethod
+#     def comer(self):
+#         print("A pessoa está comendo")
+#     @abstractmethod
+#     def andar(self):
+#         print("A pessoa está andando")
+#     @abstractmethod
+#     def dormir(self):
+#         print("A pessoa está dormindo")
+
+# class Estudante(Pessoa):
+#     def comer(self):
+#         return super().comer()
+#     def andar(self):
+#         return super().andar()
+#     def dormir(self):
+#         return super().dormir()
+    
+# joao = Estudante()
+# joao.comer()
+# joao.andar()
+# joao.dormir()
+
+
+
+# # Questão 1
+
+# class Pessoa(ABC):
+#     @abstractmethod
+#     def falar(self):
+#         print("A pessoa está falando.")
+#     @abstractmethod
+#     def andar(self):
+#         print("A pessoa está andando.")
+#     @abstractmethod
+#     def comer(self):
+#         print("A pessoa está comendo.")
+
+# class Funcionario(Pessoa):
+#     def falar(self):
+#         return super().falar()
+#     def andar(self):
+#         return super().andar()
+#     def comer(self):
+#         return super().comer()
+
+# class Aluno(Pessoa):
+#     def falar(self):
+#         return super().falar()
+#     def andar(self):
+#         return super().andar()
+#     def comer(self):
+#         return super().comer()
+    
+# jullyssan = Funcionario()
+# jullyssan.comer()
+# jully = Aluno()
+# jully.andar()
+
+
+# Questão 2
+
+# class Pessoa(ABC):
+#     @abstractmethod
+#     def falar(self):
+#         ...
+#     @abstractmethod
+#     def andar(self):
+#         ...
+#     @abstractmethod
+#     def comer(self):
+#         ...
+
+# class Funcionario(Pessoa):
+#     def falar(self):
+#         print("Ele está falando.")
+#     def andar(self):
+#         print("Ele está andando.")
+#     def comer(self):
+#         print("Ele está comendo.")
+
+# class Aluno(Pessoa):
+#     def falar(self):
+#         print("Ele falou.")
+#     def andar(self):
+#         print("Ele andou.")
+#     def comer(self):
+#         print("Ele comeu.")
+    
+# jullyssan = Funcionario()
+# jullyssan.falar()
+# jully = Aluno()
+# jully.andar()
---
+++ Aulas-Básico/Testando .py
@@ -0,0 +1,23 @@
+# Calcula o próximo ID disponível
+proximo_id = max(pets.keys()) + 1 if pets else 1
+# Função para cadastrar um novo pet
+def cadastrar_pet():
+    global proximo_id
+    nome = input("Nome do pet: ")
+    idade = int(input("Idade do pet: "))
+    raca = input("Raça do pet: ")
+    especie = input("Espécie do pet: ")
+
+    novo_pet = {
+        'nome': nome,
+        'idade': idade,
+        'especie': especie,
+        'raca': raca,
+        'adotado': False
+    }
+    pets[proximo_id] = novo_pet
+    print(f"\nPet '{nome}' cadastrado com sucesso! ID: {proximo_id}")
+    proximo_id += 1 # Incrementa o próximo ID para o próximo cadastro
+
+
+    
