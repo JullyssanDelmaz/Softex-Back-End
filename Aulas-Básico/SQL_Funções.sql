e200c4598787dabf23d110923c6b71d9fe94c1ed
Jullyssan Delmaz <jullydelmaz@yahoo.com.br>
2025-10-30 22:18:03 +0000

    Atividade de Funções

---
+++ Aulas-Básico/SQL_Funções.sql
@@ -0,0 +1,24 @@
+-- Questão 1
+
+-- SELECT COUNT (*) AS id
+-- FROM Aluno;
+
+-- Questão 2
+
+-- SELECT MIN(mensalidade) AS menor_mensalidade
+-- FROM Curso;
+
+-- Questão 3
+
+-- SELECT MAX(nota1) AS maior_nota
+-- FROM Aluno;
+
+-- Questão 4
+
+-- SELECT SUM(mensalidade) AS soma_mensalidades
+-- FROM Curso;
+
+-- Questão 5
+
+-- SELECT AVG(nota2) AS media_nota2
+-- FROM Aluno;
